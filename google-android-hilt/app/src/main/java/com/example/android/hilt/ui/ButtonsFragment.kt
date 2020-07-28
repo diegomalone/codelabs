2b286cd81276bb545bbaeac32b868a3facbb8ce0
Diego Malone <diegomalone@gmail.com>
2020-07-28 19:23:07 -0300

    Add multiple implementations of the same interface with qualifiers

--- google-android-hilt/app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
+++ google-android-hilt/app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
@@ -23,7 +23,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.android.hilt.R
-import com.example.android.hilt.data.LoggerLocalDataSource
+import com.example.android.hilt.data.LoggerDataSource
+import com.example.android.hilt.di.InMemoryLogger
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,7 +37,9 @@ import javax.inject.Inject
 class ButtonsFragment : Fragment() {
 
     @Inject
-    lateinit var logger: LoggerLocalDataSource
+    @InMemoryLogger
+    lateinit var logger: LoggerDataSource
+
     @Inject
     lateinit var navigator: AppNavigator
 
